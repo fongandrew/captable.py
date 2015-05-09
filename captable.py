20b969488625d9a92862f3ea0085de71a80d9365
Andrew F <id@andrewfong.com>
2015-05-09 11:53:22 -0700

    Initial class and test

---
+++ captable.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+class Captable(object):
+	"""Represents a captable for a company"""
+	
+	def __init__(self, *args):
+		"""Can be constructed with a list of initial security types"""
+        pass
---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+import captable
+
+class CaptableTests(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(1)
+
+if __name__ == '__main__':
+    unittest.main()
