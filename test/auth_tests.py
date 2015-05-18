e8ed3ed809c4e49dc333bd8a4ad3222c6ab9143a
Andrew F <id@andrewfong.com>
2015-05-18 14:01:14 -0700

    Multiple auth tests, delta

--- test/auth_tests.py
+++ test/auth_tests.py
@@ -15,7 +15,7 @@ class AuthTests(unittest.TestCase):
         """Initialize a blank captable and authorize multiple classes of
         securities"""
         self.table = captable.CapTable()
-        self.table.record_multi(datetime.datetime.now(),
+        self.table.record_multi(datetime.datetime(2015,5,9),
             captable.AuthTransaction(security=CLASS_A_COMMON, amount=1000000),
             captable.AuthTransaction(security=CLASS_B_COMMON, amount=500000))
     
@@ -35,57 +35,49 @@ class AuthTests(unittest.TestCase):
         self.assertEqual(amountsB.outstanding, 0)
 
 
+class MultipleAuthTests(AuthTests):
+    def setUp(self):
+        """Change number of shares of stock"""
+        super(MultipleAuthTests, self).setUp()
+        self.table.record(datetime.datetime(2015,5,10),
+            captable.AuthTransaction(security=CLASS_B_COMMON, amount=750000))
 
-# class MultipleAuthTests(AuthTests):
-#     def setUp(self):
-#         """Change number of shares of stock"""
-#         super(MultipleAuthTests, self).setUp()
-#         self.table.record_txn(
-#             captable.AuthTransaction(
-#                 cls=ClassBCommon,
-#                 amount=750000,
-#                 txn_datetime=datetime.datetime(2015,5,10)))
-#         self.table_state = self.table.process()
+    def test_authorize_amount(self):
+        """Old classes should be unchanged -- specified class should have new
+        updated numbers"""
+        amountsA = self.table.state[CLASS_A_COMMON]
+        self.assertEqual(amountsA.authorized, 1000000)
+        self.assertEqual(amountsA.issuable, 1000000)
+        self.assertEqual(amountsA.unreserved, 1000000)
+        self.assertEqual(amountsA.outstanding, 0)
 
-#     def test_authorize_amount(self):
-#         """Old classes should be unchanged -- specified class should have new
-#         updated numbers"""
-#         amountsA = self.table_state.get_amounts(ClassACommon)
-#         self.assertEqual(amountsA.authorized, 1000000)
-#         self.assertEqual(amountsA.issued, 0)
-#         self.assertEqual(amountsA.outstanding, 0)
-#         self.assertEqual(amountsA.reserved, 0)
+        amountsB = self.table.state[CLASS_B_COMMON]
+        self.assertEqual(amountsB.authorized, 750000)
+        self.assertEqual(amountsB.issuable, 750000)
+        self.assertEqual(amountsB.unreserved, 750000)
+        self.assertEqual(amountsB.outstanding, 0)
 
-#         amountsB = self.table_state.get_amounts(ClassBCommon)
-#         self.assertEqual(amountsB.authorized, 750000)
-#         self.assertEqual(amountsB.issued, 0)
-#         self.assertEqual(amountsB.outstanding, 0)
-#         self.assertEqual(amountsB.reserved, 0)
 
-# class DeltaAuthTests(AuthTests):
-#     def setUp(self):
-#         """Authorize a delta change to the number of shares of stock"""
-#         super(DeltaAuthTests, self).setUp()
-#         self.table.record_txn(
-#             captable.AuthTransaction(
-#                 cls=ClassBCommon,
-#                 delta=250000,
-#                 txn_datetime=datetime.datetime(2015,5,10)))
-#         self.table_state = self.table.process()
+class DeltaAuthTests(AuthTests):
+    def setUp(self):
+        """Authorize a delta change to the number of shares of stock"""
+        super(DeltaAuthTests, self).setUp()
+        self.table.record(datetime.datetime(2015,5,10),
+            captable.AuthTransaction(security=CLASS_B_COMMON, delta=250000))
 
-#     def test_authorize_amount(self):
-#         """Old classes should be unchanged -- specified class should have new
-#         updated numbers"""
-#         amountsA = self.table_state.get_amounts(ClassACommon)
-#         self.assertEqual(amountsA.authorized, 1000000)
-#         self.assertEqual(amountsA.issued, 0)
-#         self.assertEqual(amountsA.outstanding, 0)
-#         self.assertEqual(amountsA.reserved, 0)
+    def test_authorize_amount(self):
+        """Old classes should be unchanged -- specified class should have new
+        updated numbers"""
+        amountsA = self.table.state[CLASS_A_COMMON]
+        self.assertEqual(amountsA.authorized, 1000000)
+        self.assertEqual(amountsA.issuable, 1000000)
+        self.assertEqual(amountsA.unreserved, 1000000)
+        self.assertEqual(amountsA.outstanding, 0)
 
-#         amountsB = self.table_state.get_amounts(ClassBCommon)
-#         self.assertEqual(amountsB.authorized, 750000)
-#         self.assertEqual(amountsB.issued, 0)
-#         self.assertEqual(amountsB.outstanding, 0)
-#         self.assertEqual(amountsB.reserved, 0)
+        amountsB = self.table.state[CLASS_B_COMMON]
+        self.assertEqual(amountsB.authorized, 750000)
+        self.assertEqual(amountsB.issuable, 750000)
+        self.assertEqual(amountsB.unreserved, 750000)
+        self.assertEqual(amountsB.outstanding, 0)
 
 # amount / delta - error check
